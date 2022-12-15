aeeeb2cd4db42a97d56b7773557f86e0b6c416a2
Pavel Smirnov <pavel.smirnov.1987@me.com>
2022-12-15 22:48:45 +0300

    #1.1 def added

--- Pavel.py
+++ Pavel.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
-with open('log.txt', 'a', encoding='utf-8') as the_log_file:
-    now = str(datetime.now())
-    the_log_file.write("\n" + now[:19] + " " + input())
+def log_update():
+    with open('log.txt', 'a', encoding='utf-8') as the_log_file:
+        now = str(datetime.now())
+        the_log_file.write("\n" + now[:19] + " " + input())
 
-
-
-
+log_update()
