e989366bc9e124049988a74992424022a90feca4
Pavel Smirnov <pavel.smirnov.1987@me.com>
2022-12-23 15:47:42 +0300

    #3 fixed enclosure, deleted temp variable

--- Pavel_hw2.py
+++ Pavel_hw2.py
@@ -15,18 +15,13 @@ def add_data():
 
     with open('first_db', 'r', encoding = 'utf-8') as file:
         current_db = json.load(file)
-        if args.key in current_db:
-            search_key = current_db.get(args.key)
-            if type(search_key) == list:
-                current_db[args.key].append(args.val)
-            else:
-                temp = current_db[args.key]
-                current_db[args.key] = []
-                current_db[args.key].append(temp)
-                current_db[args.key].append(args.val)
+        if args.key in current_db and type(current_db.get(args.key)) == list:
+            current_db[args.key].append(args.val)
+        elif args.key in current_db:
+            current_db[args.key] = current_db[args.key].split()
+            current_db[args.key].append(args.val)
         else:
             current_db.update(info)
-
 
     with open('first_db', 'w', encoding='utf-8') as file:
         json.dump(current_db, file, indent=3)
